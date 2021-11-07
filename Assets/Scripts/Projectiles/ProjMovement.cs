1194018e8081030dc2b40b2fd236f388f38736ed
tower <towergame.epic@gmail.com>
2021-11-07 19:00:57 +0200

    Beautiful particles :)

--- Assets/Scripts/Projectiles/ProjMovement.cs
+++ Assets/Scripts/Projectiles/ProjMovement.cs
@@ -14,16 +14,22 @@ public class ProjMovement : MonoBehaviour
 	GameObject graphObj;
 	Graph graphComp;
 
-	private Vector2 NextPos(float xDist) {
+	private Vector2 NextPos(float xDist)
+	{
 		float nextX = transform.position.x + xDist;
 		float funcVal = moveLUT.ValueAt(nextX);
 		float nextY;
 
-		if (float.IsNaN(funcVal)) {
+		if (float.IsNaN(funcVal))
+		{
 			nextY = levelWidth * 2;
-		} else if (Mathf.Abs(funcVal) > levelWidth) {
+		}
+		else if (Mathf.Abs(funcVal) > levelWidth)
+		{
 			nextY = levelWidth * 1.1f * Mathf.Sign(funcVal);
-		} else {
+		}
+		else
+		{
 			nextY = funcVal;
 		}
 
@@ -32,16 +38,22 @@ public class ProjMovement : MonoBehaviour
 		return new Vector2(nextX, nextY);
 	}
 
-	private void Start() {
+	private void Start()
+	{
 		direction = Random.Range(0, 2) == 0 ? -1 : 1;
 
 		moveFunction = FunctionGenerator.Generate(1);
 		moveLUT = new LUT(moveFunction, new Vector2(-levelWidth * 1.1f, levelWidth * 1.1f));
 		// Debug.Log(moveFunction.GetNotation());
 
-		Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+		Color color = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), 1f);
 
 		GetComponent<SpriteRenderer>().color = color;
+		Material m = new Material(GetComponent<TrailRenderer>().materials[0]);
+		GetComponent<SpriteRenderer>().material = m;
+		GetComponent<SpriteRenderer>().material.SetColor("_BaseColor", color);
+		GetComponent<TrailRenderer>().materials[0] = m;
+		GetComponent<TrailRenderer>().materials[0].SetColor("_BaseColor", color);
 
 		graphObj = Instantiate(graphPrefab, new Vector2(0, 0), new Quaternion());
 		graphComp = graphObj.GetComponent<Graph>();
@@ -54,10 +66,14 @@ public class ProjMovement : MonoBehaviour
 		transform.position = new Vector2(levelWidth * 1.1f * -direction, 0);
 	}
 
-	private void Update() {
-		if (transform.position.x * direction < levelWidth) {
+	private void Update()
+	{
+		if (transform.position.x * direction < levelWidth)
+		{
 			transform.position = NextPos(speed * Time.deltaTime * direction);
-		} else {
+		}
+		else
+		{
 			Destroy(graphObj);
 			Destroy(gameObject);
 		}
